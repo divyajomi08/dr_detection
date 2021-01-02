3077e13abb5ac64a24258286c4edd720f67a14c0
divyajomi08 <divyajomi01@gmail.com>
2021-01-02 12:39:31 +0530

    form added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrescriptionCard from './components/Cards/PrescriptionCard';
+import PrescriptionCard from './components/PrescriptionCard';
 import PrescriptionList from './components/PrescriptionList';
+import Detailsform from './components/Detailsform';
 
 const App = () => {
 
   return (
     <div className="ui container">
-      <PrescriptionCard />
-      <PrescriptionList />
+      {/* <PrescriptionCard />
+      <PrescriptionList /> */}
+      <Detailsform/>
       
     </div>
 
