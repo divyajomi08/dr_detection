f2fcb767b5f1a3262ec4a1db2e9f2b56269865ac
Melvin Mathew <melvinmathew0102@gmail.com>
2021-01-03 10:27:12 +0530

    optimized

--- src/index.js
+++ src/index.js
@@ -7,26 +7,20 @@ import Patient from './views/PatientAnalysis';
 import Support from './views/Support';
 import Prescription from './views/Prescription';
 import './App.css';
-import Detailsform from './components/Detailsform';
 
 const App = () => {
-  return (   
+  return (
     <div>
-      {/* <DashboardLayout /> */}
+      <DashboardLayout />
       {/*Test Components Here*/}
-<<<<<<< HEAD
-      <BrowserRouter>
-=======
-      <Detailsform/>
       {/* <BrowserRouter>
->>>>>>> ddaabb5a9c3f3c3d0aafa0b3ead36a0d02664fcb
         <Switch>
           <Route path='/' exact component={Dashboard} />
           <Route path='/prescription' component={Prescription} />
           <Route path='/patient' component={Patient} />
           <Route path='/support' component={Support} />
         </Switch>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
 
   );
