30fd44b5208c4af4ea9cc5671b04bc5bde885789
Melvin Mathew <melvinmathew0102@gmail.com>
2021-01-04 14:45:20 +0530

    Logo added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,25 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 const HeaderView = () => {
-    const [profile,setProfile]=useState("item");
-    const [logout,setlogout]=useState("ui item active");
- 
+    const [profile, setProfile] = useState("item");
+    const [logout, setlogout] = useState("ui item");
+
     return (
-        <div className="ui secondary pointing menu" style={{margin:10}}>
-            <a className={profile} onClick={()=>{setProfile("ui item active");setlogout("item");}}>
-                Profile
-            </a>
-            <div className="right menu" onClick={()=>{setProfile("item");setlogout("ui item active");}}>
-                <a className={logout} >
-                    Logout
-                </a>
+        <div className="ui secondary menu" >
+            <img class="ui image" alt="Logo"  style={{ marginLeft:10,width: 60, height: 60 }} src="https://play-lh.googleusercontent.com/83gl9FJDgo-bOdKb5hteRVWxCK_6ZKkAMPJCWxMwPsO1SHbiWvqvwgPwmHfUuyzpMQ=s180-rw"></img>
+            <div className="right menu" >
+                <div class="item">
+                    <a className={profile} onClick={() => { setProfile("ui item active"); setlogout("item"); }}>
+                        Profile
+                    </a>
+                </div>
+                <div className="item" onClick={() => { setProfile("item"); setlogout("ui item active"); }}>
+                    <a className={logout} >
+                        Logout
+                    </a>
+                </div>
             </div>
+
         </div>
     );
 };
